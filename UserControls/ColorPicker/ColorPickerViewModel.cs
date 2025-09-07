6239900a1a099c7fc74afca8556c57e5b8032a55
laiqun <laiqun007@gmail.com>
2025-09-07 22:36:17 +0800

    Add color pickup user control.

---
+++ UserControls/ColorPicker/ColorPickerViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ctxmgr.UserControls.ColorPicker
+{
+    public partial class ColorPickerViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(SelectedColor))]
+        private byte a;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(SelectedColor))]
+        private byte r;
+        
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(SelectedColor))]
+        private byte g;
+        
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(SelectedColor))]
+        private byte b;
+
+        public SolidColorBrush SelectedColor => new SolidColorBrush(Color.FromArgb(A, R, G, B));
+        public ColorPickerViewModel() {
+            a = 0;
+            r = 0;
+            g = 0;
+            b = 0;
+        }
+    }
+}
